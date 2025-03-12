b9c02546aa7070d4143ec7124d15a00812d4be14
Marilu-fra-92 <marilenafracchi@gmail.com>
2025-03-12 16:08:12 +0100

    aggiunte views in lista uffici

--- src/app/lista-uffici/lista-uffici.component.ts
+++ src/app/lista-uffici/lista-uffici.component.ts
@@ -14,30 +14,57 @@ import { SelezionaImpiegatoDialogComponent } from '../seleziona-impiegato-dialog
 export class ListaUfficiComponent implements OnInit {
   displayedColumns: string[] = ['codUff', 'nomeUff', 'sede', 'indirizzo', 'numInterno', 'action'];
   dataSource: any[] = [];
+  dataSourceConteggio: any[] = [];  // Nuovo array per i dati del conteggio dei dipendenti per ufficio
+  currentView: string = 'uffici';  // Variabile per gestire la vista corrente
 
   @ViewChild(MatSort) sort: MatSort | undefined;
 
   constructor(private http: HttpClient, private ufficioService: UfficioService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
-    this.getUffici();
+    this.getUffici();  // Recupera gli uffici all'inizio
   }
 
+  // Funzione per recuperare gli uffici
   getUffici() {
     this.http.get<any>('http://localhost:3000/ufficio').subscribe(
       (response) => {
-        this.dataSource = response.data;
+        this.dataSource = response.data;  // Popola la lista degli uffici
       },
       (error) => console.error('Errore nel recupero degli uffici:', error)
     );
   }
 
+  /* Funzione per recuperare il conteggio dei dipendenti per ufficio
+  getConteggioDipendenti() {
+    this.http.get<any>('http://localhost:3000/ufficio/conteggio-dipendenti').subscribe(
+      (response) => {
+        this.dataSourceConteggio = response.data;  // Popola il conteggio dei dipendenti
+      },
+      (error) => console.error('Errore nel recupero del conteggio dei dipendenti:', error)
+    );
+  }  */
+
+    getConteggioDipendenti() {
+      throw new Error('Method not implemented.');
+    }
+
+  // Funzione per cambiare la vista
+  setView(view: string) {
+    this.currentView = view;  // Imposta la vista attiva
+    if (view === 'dipendenti-per-ufficio') {
+      this.getConteggioDipendenti();  // Recupera i dati dei dipendenti per ufficio quando si cambia alla vista dei dipendenti
+    }
+  } 
+  
+
+  // Funzione per associare un impiegato a un ufficio
   associaImpiegatodaUff(codUff: string) {
     const dialogRef = this.dialog.open(SelezionaImpiegatoDialogComponent, {
       width: '400px',
       data: { codUff }
     });
-  
+
     dialogRef.afterClosed().subscribe((impiegatoId) => {
       if (impiegatoId) {
         // Una sola chiamata al backend, che viene già fatta nel componente SelezionaImpiegatoDialogComponent
@@ -46,6 +73,4 @@ export class ListaUfficiComponent implements OnInit {
       }
     });
   }
-  
-
 }
